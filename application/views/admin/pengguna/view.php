ee41a0debed93d6070c7092762269e0ea024f91c
tantokaya <tanyatantociputat@gmail.com>
2015-05-20 07:47:36 +0700

    buat branch dev

--- application/views/admin/pengguna/view.php
+++ application/views/admin/pengguna/view.php
@@ -26,6 +26,7 @@
                         <td ><?php echo $db['nama_lengkap']; ?></td>
                         <td ><?php echo $db['id_level'].' - '.$level; ?></td>
                         <td style="text-align: center; width: 130px;">
+                            <?php if($this->session->userdata('id_level')=='01'){ ?>
                             <a href="<?php echo base_url();?>index.php/pengguna/edit/<?php echo $db['username'];?>" rel="tooltip" title="Edit">
                                 <button class="btn btn-primary btn-xs"><i class="fa fa-edit"></i> Ubah</button>
                             </a>
@@ -33,6 +34,7 @@
                                onClick="return confirm('Anda yakin ingin menghapus data ini?')" rel="tooltip" title="Hapus">
                                 <button class="btn btn-danger btn-xs"><i class="fa fa-trash-o"></i> Hapus</button>
                             </a>
+                            <?php } ?>
                         </td>
                     </tr>
                     <?php
